7eb9112ac6e34cd4aa910fecf588e65cabba7e32
lucikelly <lucikelly2001@gmail.com>
2024-08-24 14:19:47 -0300

    add: component projectList

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import Footer from "../components/Footer/Footer"
 import Header from "../components/Header/Header"
 import Hero from "../components/Hero/Hero"
+import ProjectList from "../components/ProjectList/ProjectList"
 
 
 function Home() {
@@ -10,8 +11,10 @@ function Home() {
     <>
     <Header/>
     <div className="container">
-    <Hero/>
+      <Hero/>
+      <ProjectList/>
     </div>
+    
     
     <Footer/>
     </>
