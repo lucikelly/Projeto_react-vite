5fb178d8d66aada5ff42305e7042132d5f698f5a
lucikelly <lucikelly2001@gmail.com>
2024-08-24 13:12:49 -0300

    component hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import Banner from "../components/Banner/Banner"
-import Button from "../components/Button/Button"
+
 import Footer from "../components/Footer/Footer"
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
 
 
 function Home() {
@@ -9,9 +9,10 @@ function Home() {
   return (
     <>
     <Header/>
-    <p>Home</p>
-    <Button buttonStyle="primary" arrow>ola</Button>
-    <Banner title="Home" image="about.jpg"/>
+    <div className="container">
+    <Hero/>
+    </div>
+    
     <Footer/>
     </>
   )
