4f1f77279dedd615ba8fd649d15804ccf2d7b92a
lucikelly <lucikelly2001@gmail.com>
2024-08-28 18:50:09 -0300

    add: menu da barra de navegação para mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom"
 import LogoDNC from "../../assets/LogoDNC.svg"
 import './Header.css'
+import Button from "../Button/Button"
+import { useState } from 'react'
 
 function Header () {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () =>{
+    setIsOpen (!isOpen)
+  }
   return(
+
     <header>
       <div className="container">
         <div className="al-center d-flex jc-space-b ">
           <Link to="/"><img src={LogoDNC}/></Link>
-          <nav>
-            <ul className="d-flex">
+          <div className="mobile-menu">
+            <Button buttonStyle="secudary" onClick={toggleMenu}>
+                Menu
+            </Button>
+          </div>
+          <nav className={`${isOpen? 'open' : ''}`}>
+            <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={toggleMenu}>
+              X
+            </Button>
+            <ul className="d-flex" >
               <li><Link to="/">Home</Link></li>
               <li><Link to="/about">About</Link></li>
               <li><Link to="/projects">Projects</Link></li>
