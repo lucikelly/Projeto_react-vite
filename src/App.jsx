637f12a0bea63686d165de6990e82cc2b492eea9
lucikelly <lucikelly2001@gmail.com>
2024-08-28 19:28:53 -0300

    add: função de ScrollToTop (na mundação de tela o scroll da pagina vai para o inicio)

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import About from './pages/About'
 import Projects from './pages/Products'
 import Contact from './pages/Contact'
 
+import ScrollToTop from './utils/ScrollToTop'
+
 function App() {
  
 
   return (
     <Router>
+      <ScrollToTop/>
       <Routes>
        <Route path="/" element={ <Home/>}></Route>
        <Route path="/about" element={<About/> }></Route>
